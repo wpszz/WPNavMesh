75cf567dde544890cf94ecf4ae55a0429fd536f2
wpszz <shenzizai@qq.com>
2019-07-02 10:30:56 +0800

    nav mesh export

---
+++ Assets/Script/NavMeshExport.cs
@@ -0,0 +1,132 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+namespace WP
+{
+    public class NavMeshExport
+    {
+        public static bool Serialize(NavMeshBuilder builder, out string json)
+        {
+            builder.Refresh();
+
+            json = "";
+
+            List<Vector3> allVerts = new List<Vector3>();
+            List<int[]> allPolys = new List<int[]>();
+            foreach (var poly in builder.polys)
+            {
+                int[] indexs = new int[poly.vertices.Count];
+                for (int i = 0; i < indexs.Length; i++)
+                    indexs[i] = allVerts.Count + i;
+                allVerts.AddRange(poly.vertices);
+                allPolys.Add(indexs);
+            }
+
+            List<Vector3> newVerts = new List<Vector3>();
+            Dictionary<int, int> indexOldToNew = new Dictionary<int, int>();
+            for (int i = 0; i < allVerts.Count; i++)
+            {
+                int existedIndex = -1;
+                for (int j = 0; j < newVerts.Count; j++)
+                {
+                    if (Vector3.Distance(allVerts[i], newVerts[j]) < 0.01)
+                    {
+                        existedIndex = j;
+                        break;
+                    }
+                }
+                if (existedIndex < 0)
+                {
+                    indexOldToNew[i] = newVerts.Count;
+                    newVerts.Add(allVerts[i]);
+                }
+                else
+                {
+                    indexOldToNew[i] = existedIndex;
+                }
+            }
+            foreach (var indexs in allPolys)
+            {
+                for (int i = 0; i < indexs.Length; i++)
+                    indexs[i] = indexOldToNew[indexs[i]];
+            }
+
+            int polyCount = allPolys.Count;
+            PolyNode[] polyNodes = new PolyNode[polyCount];
+            for (int i = 0; i < polyCount; i++)
+            {
+                var indexs = allPolys[i];
+                PolyNode node = new PolyNode();
+                node.vertexs = new Vector2[indexs.Length];
+                for (int j = 0; j < indexs.Length; j++)
+                    node.vertexs[j] = new Vector2(newVerts[indexs[j]].x, newVerts[indexs[j]].z);
+                node.center = Math.CalculatePolyCenter2D(node.vertexs);
+                polyNodes[i] = node;
+                node.index = i;
+
+                // only support convex ploygons
+                if (!Math.IsConvexPoly(node.vertexs))
+                {
+                    Debug.LogError("Polygon " + builder.polys[i].name + " is not a convex polygon.", builder.polys[i]);
+                    return false;
+                }
+
+                // make sure clockwise order
+                if (!Math.IsClockwise2D(node.vertexs[0], node.vertexs[1], node.vertexs[2]))
+                {
+                    System.Array.Reverse(node.vertexs);
+                    System.Array.Reverse(indexs);
+                }
+            }
+            // find neighbors
+            List<PolyNode> neighbors = new List<PolyNode>();
+            List<int> neighborEdges = new List<int>();
+            for (int i = 0; i < polyCount; i++)
+            {
+                var indexs = allPolys[i];
+                neighbors.Clear();
+                neighborEdges.Clear();
+                for (int j = 0; j < polyCount; j++)
+                {
+                    if (i == j)
+                        continue;
+                    var indexs2 = allPolys[j];
+                    int neighborEdge = -1;
+                    for (int m = 0; m < indexs.Length; m++)
+                    {
+                        int e1 = indexs[m];
+                        int e2 = indexs[m + 1 >= indexs.Length ? 0 : m + 1];
+                        for (int n = 0; n < indexs2.Length; n++)
+                        {
+                            int e3 = indexs2[n];
+                            int e4 = indexs2[n + 1 >= indexs2.Length ? 0 : n + 1];
+                            if (e1 == e3 && e2 == e4 || e1 == e4 && e2 == e3)
+                            {
+                                neighborEdge = m;
+                                break;
+                            }
+                        }
+                        if (neighborEdge >= 0)
+                            break;
+                    }
+                    if (neighborEdge >= 0)
+                    {
+                        neighbors.Add(polyNodes[j]);
+                        neighborEdges.Add(neighborEdge);
+                    }
+                }
+                polyNodes[i].neighbors = neighbors.ToArray();
+                polyNodes[i].neighborEdges = neighborEdges.ToArray();
+            }
+
+            NavMesh navMesh = new NavMesh();
+            navMesh.polyNodes = polyNodes;
+
+            json = NavMesh.Serialize(navMesh);
+            Debug.Log(json);
+
+            return true;
+        }
+    }
+}
--- Assets/Script/NavMeshTest.cs
+++ Assets/Script/NavMeshTest.cs
@@ -7,11 +7,15 @@ namespace WP
     [ExecuteInEditMode]
     public class NavMeshTest : MonoBehaviour
     {
-        private PolyNode[] polyNodes;
- 
         public NavMeshBuilder builder;
         public Transform startTarget;
         public Transform endTarget;
+        [Range(-10f, 10f)]
+        public float offsetHeight = 0f;
+        [Range(20, 100)]
+        public int splitCount = 20;
+
+        NavMesh navMesh;
 
         private float testHeight;
         private Vector2 testMin;
@@ -27,7 +31,7 @@ namespace WP
 
         void Update()
         {
-            if (polyNodes == null)
+            if (navMesh == null)
                 InitNavMesh();
 
             if (startTarget && endTarget)
@@ -36,12 +40,14 @@ namespace WP
 
         void OnDrawGizmos()
         {
-            if (polyNodes != null)
+            if (navMesh != null)
             {
+                float showHeight = testHeight + offsetHeight;
+
                 Gizmos.color = Color.red;
-                for (int i = 0; i < polyNodes.Length; i++)
+                for (int i = 0; i < navMesh.polyNodes.Length; i++)
                 {
-                    PolyNode node = polyNodes[i];
+                    PolyNode node = navMesh.polyNodes[i];
                     Vector3 size = Vector3.one * 0.1f;
                     if (testPathNodeEnd != null)
                     {
@@ -57,14 +63,14 @@ namespace WP
                         }
                         while (tmp != null);
                     }
-                    Gizmos.DrawCube(new Vector3(node.center.x, testHeight, node.center.y), size);
+                    Gizmos.DrawCube(new Vector3(node.center.x, showHeight, node.center.y), size);
                 }
 
                 Gizmos.color = Color.green;
                 float width = testMax.x - testMin.x;
                 float height = testMax.y - testMin.y;
-                int row = 20;
-                int column = 20;
+                int row = splitCount;
+                int column = splitCount;
                 float deltaColumn = width / column;
                 float deltaRow = height / row;
                 Vector2 p = Vector2.zero;
@@ -73,9 +79,9 @@ namespace WP
                     {
                         p.x = testMin.x + deltaColumn * j;
                         p.y = testMin.y + deltaRow * i;
-                        if (GetInsidePolyIndex(p) >= 0)
+                        if (navMesh.GetInsidePolyIndex(p) >= 0)
                         {
-                            Gizmos.DrawRay(new Vector3(p.x, testHeight, p.y), Vector3.up);
+                            Gizmos.DrawRay(new Vector3(p.x, showHeight, p.y), Vector3.up);
                         }
                     }
 
@@ -84,11 +90,11 @@ namespace WP
                     Gizmos.color = Color.blue;
                     for (int i = 0; i < testPath.Length; i++)
                     {
-                        Gizmos.DrawCube(new Vector3(testPath[i].x, testHeight, testPath[i].y), new Vector3(0.2f, 1f, 0.2f));
+                        Gizmos.DrawCube(new Vector3(testPath[i].x, showHeight, testPath[i].y), new Vector3(0.2f, 1f, 0.2f));
                         if (i + 1 < testPath.Length)
                         {
-                            Gizmos.DrawLine(new Vector3(testPath[i].x, testHeight + 0.1f, testPath[i].y), 
-                                new Vector3(testPath[i + 1].x, testHeight + 0.1f, testPath[i + 1].y));
+                            Gizmos.DrawLine(new Vector3(testPath[i].x, showHeight + 0.1f, testPath[i].y), 
+                                new Vector3(testPath[i + 1].x, showHeight + 0.1f, testPath[i + 1].y));
                         }
                     }
                 }
@@ -99,136 +105,24 @@ namespace WP
         {
             if (builder == null)
                 return;
-            builder.Refresh();
 
-            List<Vector3> allVerts = new List<Vector3>();
-            List<int[]> allPolys = new List<int[]>();
-            foreach (var poly in builder.polys)
-            {
-                int[] indexs = new int[poly.vertices.Count];
-                for (int i = 0; i < indexs.Length; i++)
-                    indexs[i] = allVerts.Count + i;
-                allVerts.AddRange(poly.vertices);
-                allPolys.Add(indexs);
-            }
-
-            List<Vector3> newVerts = new List<Vector3>();
-            Dictionary<int, int> indexOldToNew = new Dictionary<int, int>();
-            for (int i = 0; i < allVerts.Count; i++)
-            {
-                int existedIndex = -1;
-                for (int j = 0; j < newVerts.Count; j++)
-                {
-                    if (Vector3.Distance(allVerts[i], newVerts[j]) < 0.01)
-                    {
-                        existedIndex = j;
-                        break;
-                    }
-                }
-                if (existedIndex < 0)
-                {
-                    indexOldToNew[i] = newVerts.Count;
-                    newVerts.Add(allVerts[i]);
-                }
-                else
-                {
-                    indexOldToNew[i] = existedIndex;
-                }
-            }
-            foreach (var indexs in allPolys)
-            {
-                for (int i = 0; i < indexs.Length; i++)
-                    indexs[i] = indexOldToNew[indexs[i]];
-            }
-
-            int polyCount = allPolys.Count;
-            polyNodes = new PolyNode[polyCount];
-            for (int i = 0; i < polyCount; i++)
-            {
-                var indexs = allPolys[i];
-                PolyNode node = new PolyNode();
-                node.vertexs = new Vector2[indexs.Length];
-                for (int j = 0; j < indexs.Length; j++)
-                    node.vertexs[j] = new Vector2(newVerts[indexs[j]].x, newVerts[indexs[j]].z);
-                node.center = Math.CalculatePolyCenter2D(node.vertexs);
-                polyNodes[i] = node;
-                node.index = i;
-
-                // only support convex ploygons
-                if (!Math.IsConvexPoly(node.vertexs))
-                {
-                    Debug.LogError(builder.polys[i].name + " is not a convex polygon.");
-                    return;
-                }
-
-                // make sure clockwise order
-                if (!Math.IsClockwise2D(node.vertexs[0], node.vertexs[1], node.vertexs[2]))
-                {
-                    System.Array.Reverse(node.vertexs);
-                    System.Array.Reverse(indexs);
-                }
-            }
-            // find neighbors
-            List<PolyNode> neighbors = new List<PolyNode>();
-            List<int> neighborEdges = new List<int>();
-            for (int i = 0; i < polyCount; i++)
-            {
-                var indexs = allPolys[i];
-                neighbors.Clear();
-                neighborEdges.Clear();
-                for (int j = 0; j < polyCount; j++)
-                {
-                    if (i == j)
-                        continue;
-                    var indexs2 = allPolys[j];
-                    int neighborEdge = -1;
-                    for (int m = 0; m < indexs.Length; m++)
-                    {
-                        int e1 = indexs[m];
-                        int e2 = indexs[m + 1 >= indexs.Length ? 0 : m + 1];
-                        for (int n = 0; n < indexs2.Length; n++)
-                        {
-                            int e3 = indexs2[n];
-                            int e4 = indexs2[n + 1 >= indexs2.Length ? 0 : n + 1];
-                            if (e1 == e3 && e2 == e4 || e1 == e4 && e2 == e3)
-                            {
-                                neighborEdge = m;
-                                break;
-                            }
-                        }
-                        if (neighborEdge >= 0)
-                            break;
-                    }
-                    if (neighborEdge >= 0)
-                    {
-                        neighbors.Add(polyNodes[j]);
-                        neighborEdges.Add(neighborEdge);
-                    }
-                }
-                polyNodes[i].neighbors = neighbors.ToArray();
-                polyNodes[i].neighborEdges = neighborEdges.ToArray();
-            }
-
-            //test
-            testHeight = newVerts[0].y;
-            testMin = new Vector2(float.MaxValue, float.MaxValue);
-            testMax = new Vector2(float.MinValue, float.MinValue);
-            foreach (var v in newVerts)
-            {
-                testMin.x = Mathf.Min(v.x, testMin.x);
-                testMin.y = Mathf.Min(v.z, testMin.y);
-                testMax.x = Mathf.Max(v.x, testMax.x);
-                testMax.y = Mathf.Max(v.z, testMax.y);
-            }
-
-            NavMesh navMesh = new NavMesh();
-            navMesh.polyNodes = polyNodes;
-
-            string testJson = NavMesh.Serialize(navMesh);
-            Debug.Log(testJson);
+            string testJson;
+            if (!NavMeshExport.Serialize(builder, out testJson))
+                return;
 
             navMesh = NavMesh.Deserialize(testJson);
-            polyNodes = navMesh.polyNodes;
+
+            testHeight = builder.polys[0].vertices[0].y;
+            testMin = new Vector2(float.MaxValue, float.MaxValue);
+            testMax = new Vector2(float.MinValue, float.MinValue);
+            foreach (var node in navMesh.polyNodes)
+                foreach (var v in node.vertexs)
+                    {
+                    testMin.x = Mathf.Min(v.x, testMin.x);
+                    testMin.y = Mathf.Min(v.y, testMin.y);
+                    testMax.x = Mathf.Max(v.x, testMax.x);
+                    testMax.y = Mathf.Max(v.y, testMax.y);
+                }
         }
 
         //==========================================================
@@ -240,33 +134,22 @@ namespace WP
             Vector2 startPos2D = new Vector2(startPos.x, startPos.z);
             Vector2 endPos2D = new Vector2(endPos.x, endPos.z);
 
-            int startIndex = GetInsidePolyIndex(startPos2D);
+            int startIndex = navMesh.GetInsidePolyIndex(startPos2D);
             if (startIndex < 0)
                 return;
-            int endIndex = GetInsidePolyIndex(endPos2D);
+            int endIndex = navMesh.GetInsidePolyIndex(endPos2D);
             if (endIndex < 0)
                 return;
 
-            PolyNode nodeStart = polyNodes[startIndex];
-            PolyNode nodeEnd = polyNodes[endIndex];
+            PolyNode nodeStart = navMesh.polyNodes[startIndex];
+            PolyNode nodeEnd = navMesh.polyNodes[endIndex];
 
-            if (!PolyAStar.FindPath(nodeStart, nodeEnd, polyNodes))
+            if (!PolyAStar.FindPath(nodeStart, nodeEnd, navMesh.polyNodes))
                 return;
 
             testPath = PolyCornerPath.FindPath2D(startPos2D, endPos2D, nodeEnd);
 
             testPathNodeEnd = nodeEnd;
-        }
-
-        int GetInsidePolyIndex(Vector2 p)
-        {
-            int polyCount = polyNodes.Length;
-            for (int i = 0; i < polyCount; i++)
-            {
-                if (Math.IsInsideConvexPoly(p, polyNodes[i].vertexs))
-                    return i;
-            }
-            return -1;
         }
     }
 }
